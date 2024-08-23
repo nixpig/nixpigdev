f89e9a3ee1250d1c938edf65fb416fa13097a4e2
nixpig <143995476+nixpig@users.noreply.github.com>
2024-08-23 09:00:12 +0100

    chore: clean resume

--- app/pages/resume.go
+++ app/pages/resume.go
@@ -56,25 +56,6 @@ func (r resumeModel) View() string {
 
 ## Experience
 
-**HR + Payroll SaaS (2018 - Present)**
-
-- _Senior Technical Lead, IC (2024 - Present)_
-- _Senior Engineering Lead (2021 - 2024)_
-- _Senior Engineer (2018 - 2021)_
-
-**Contractor (2016 - 2018)** • _Front End Developer_
-
-**Design agency (2015 - 2016)** • _Front End Developer_
-
-**Travel booking website (2014 - 2015)** • _Front End Developer_
-
-**Freelance (2011 - 2014)** • _Web Developer_
-
-## Education
-
-**University (2012 - 2014)** • _Engineering, Foundation Deg._
-
-**College (2011 - 2012)** • _ICT, Access to HE_
 				`, r.contentWidth),
 	}, "")
 }
